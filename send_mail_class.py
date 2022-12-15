eabdb37fecf326c3765db04180d95e9479b8bdfa
feng_land <31641981+fengland@users.noreply.github.com>
2022-12-15 16:04:39 +0800

    把message设置成保护属性
    _  underscore
    -message: protected variables
    __message: private variables

--- send_mail_class.py
+++ send_mail_class.py
@@ -14,42 +14,39 @@ class SendMail:
         # 第三方SMTP服务
         self.mail_host = 'smtp.qq.com'
         self.mail_user = '375540509@qq.com'
-        self.mail_pass = 'qwrwasdfkfl;rlkqwr'
+        self.mail_pass = 'xuhadtxwcmxibhgc'
         self.sender = '375540509@qq.com'
         self.receivers = ['jiankong@npool.com']
 
         # 创建一个带附件的实例
-        self.message = MIMEMultipart()
-        self.message['From'] = '375540509@qq.com'
-        self.message['To'] = 'devops'
+        self._message = MIMEMultipart()
+        self._message['From'] = '375540509@qq.com'
+        self._message['To'] = 'devops'
 
         # 邮件标题
         self.subject = 'POST账户余额不足'
-        self.message['Subject'] = Header(self.subject,'utf-8')
+        self._message['Subject'] = Header(self.subject,'utf-8')
 
         # 邮件正文
         self.MAIL_CONTENT = """
         """
 
-
     def mail_content(self,MAIL_CONTENT):
-        self.message.attach(MIMEText(f'{MAIL_CONTENT}', 'plain', 'utf-8'))
+        self._message.attach(MIMEText(f'{MAIL_CONTENT}', 'plain', 'utf-8'))
 
     # 添加附件
     def add_attachment(self,attachment_name):
         part = MIMEApplication(open(attachment_name, 'rb').read())
         part.add_header('Content-Disposition', 'attachment', filename=attachment_name)
-        self.message.attach(part)
+        self._message.attach(part)
 
     # 发送邮件
     def send_mail(self):
         smtpObj = smtplib.SMTP()
         smtpObj.connect(self.mail_host, 25)
         smtpObj.login(self.mail_user, self.mail_pass)
-        smtpObj.sendmail(self.sender, self.receivers, self.message.as_string())
+        smtpObj.sendmail(self.sender, self.receivers, self._message.as_string())
         smtpObj.quit()
-
-
 
 if __name__ == '__main__':
     # add_attachment()
