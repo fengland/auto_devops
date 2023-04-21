e8573c54f635cc8cbc7e6f545e2870e615f309cc
feng_land <31641981+fengland@users.noreply.github.com>
2023-04-21 16:40:59 +0800

    使用openssl password也可以
    使用openssl password -1生成的密码也可以

--- hpool_miner/create_pass.py
+++ hpool_miner/create_pass.py
@@ -3,3 +3,10 @@ from passlib.hash import sha512_crypt
 
 hash = sha512_crypt.encrypt("123456")
 print(hash)
+
+
+
+# 创建加密的登录用户，password参数必须要是经过加密的字符串。-1是MD5加密
+# echo 'linzfn' | openssl passwd -1 -stdin
+$1$FNGUGf3H$5rfXpwXqlkL9HDC88uWRA1
+# ansible test -m user -a 'name=leon password="$1$FNGUGf3H$5rfXpwXqlkL9HDC88uWRA1"'
